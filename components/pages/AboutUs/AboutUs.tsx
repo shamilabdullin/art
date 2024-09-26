f7f0006d12b840a2af9a482eca3ab02a70ab2630
Shamil Abdullin <abdullin.shamil100@gmail.com>
2024-09-26 22:16:34 +0300

    about us page updated

--- components/pages/AboutUs/AboutUs.tsx
+++ components/pages/AboutUs/AboutUs.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-// Stores, utils, libs
+// Components
 import Image from 'next/image'
+
+// Stores, utils, libs
 import shamil from 'public/shamil.jpg'
 
 // CSS
@@ -9,31 +11,34 @@ import styles from './AboutUs.module.sass'
 
 export default function AboutUs():JSX.Element {
   return (
-	<div className={styles.aboutUs_page}>
-		<div className={styles.content}>
-			<div className={styles.container}>
-				<Image src={shamil} alt='Shamil' className={styles.image}/>
-			</div>
-			<div className={styles.article}>
-				<p>
-					Hello, my name is Abdullin Shamil, and this site is my project.
-				</p>
-				<p>
-					I use collection from database of the Art Institite of Chicago.
-				</p>
-				<p>
-					I create it for artists and those, who interested in art.
-				</p>
-				<p>
-					I hope you will enjoy it.
-				</p>
-				<p style={{'marginTop' : '24px'}}>
-					<strong>
-						Github project link <a href="https://github.com/shamilabdullin/art">https://github.com/shamilabdullin/art</a>
-					</strong>
-				</p>
+		<div className={styles.aboutUs_page}>
+			<div className={styles.content}>
+				<div className={styles.container}>
+					<Image src={shamil} alt='Shamil' className={styles.image} />
+				</div>
+				<div className={styles.article}>
+					<p>
+						Hello, my name is Abdullin Shamil, and this site is my project.
+					</p>
+					<p>
+						I use collection from database of the Art Institite of Chicago.
+					</p>
+					<p>
+						I create it for artists and those, who interested in art.
+					</p>
+					<p>
+						I hope you will enjoy it.
+					</p>
+					<p style={{'marginTop' : '24px'}}>
+						<strong>
+							Github project link&nbsp;
+							<a href="https://github.com/shamilabdullin/art">
+								https://github.com/shamilabdullin/art
+							</a>
+						</strong>
+					</p>
+				</div>
 			</div>
 		</div>
-	</div>
   )
 }
